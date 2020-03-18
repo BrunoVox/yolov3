2ff0c37a203f50434bcc072e977c0c42a6af6fbf
Bruno Oliveira <31042118+BrunoVox@users.noreply.github.com>
2020-03-17 21:25:15 -0300

    Change import onnx
    This pull is to prevent dynamic loader issues, which caused "Segmentation fault (core dumped)" errors when onnx.checker.check_model() was called in some cases.

--- detect.py
+++ detect.py
@@ -1,4 +1,5 @@
 import argparse
+import onnx
 from sys import platform
 
 from models import *  # set ONNX_EXPORT in models.py
@@ -48,7 +49,6 @@ def detect(save_img=False):
         torch.onnx.export(model, img, f, verbose=False, opset_version=11)
 
         # Validate exported model
-        import onnx
         model = onnx.load(f)  # Load the ONNX model
         onnx.checker.check_model(model)  # Check that the IR is well formed
         print(onnx.helper.printable_graph(model.graph))  # Print a human readable representation of the graph
